d90ef74543408472a7286078ba4b6ba381ee765c
Golo_er38@student.itstep.org <Golo_er38@student.itstep.org>
2022-02-19 17:06:34 +0200

    Добавьте файлы проекта.

---
+++ LambdaExpression/LambdaExpression.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class print
+{
+public:
+	void operator() (int element)
+	{
+		cout << element << endl;
+	}
+};
+
+int main()
+{
+	vector<int> v = {1,2,3,4,5};
+	for_each(v.begin(), v.end(), print());
+	system("pause");
+	return 0;
+}
